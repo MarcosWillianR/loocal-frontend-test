bb5134f3eac6abbad65b8c95cc178cea36acf8f1
Marcos Willian <markusuuuu@gmail.com>
2020-10-12 01:42:42 -0300

    replace illustration format

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import illustrationTop from '../../assets/Illustration-top.png';
+import illustrationTop from '../../assets/Illustration-top.svg';
 
 import { Container, ContentWrapper, DescriptionContent } from './styles';
 
