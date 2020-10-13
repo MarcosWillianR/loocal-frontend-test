f882cafdc180242e793c462f8281cc19f2f68102
Marcos Willian <markusuuuu@gmail.com>
2020-10-13 00:07:37 -0300

    get data from an API

--- src/components/TestimonialsList/index.tsx
+++ src/components/TestimonialsList/index.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
+
+import api from '../../services/apiClient';
 
 import starIcon from '../../assets/icons/star.svg';
 
@@ -8,41 +10,69 @@ import Arrow from './Arrow';
 
 import { Container, ListItem, Title, StarsCount } from './styles';
 
+interface Testimonial {
+  name: string;
+  website: string;
+  image: string;
+}
+
+interface TestimonialResponse {
+  data: {
+    firstname: string;
+    lastname: string;
+    website: string;
+    image: string;
+  }[];
+}
+
 const TestimonialsList: React.FC = () => {
+  const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
+
+  useEffect(() => {
+    api.get<TestimonialResponse>('persons').then(response => {
+      setTestimonials(
+        response.data.data.map(({ firstname, website, image, lastname }) => ({
+          name: `${firstname} ${lastname}`,
+          image,
+          website,
+        })),
+      );
+    });
+  }, []);
+
   return (
     <Container>
-      <ScrollMenu
-        data={[1, 2, 3, 4].map(i => (
-          <ListItem key={i}>
-            <div>
-              <img
-                src="https://randomwordgenerator.com/img/picture-generator/52e6d34b4a57a814f1dc8460962e33791c3ad6e04e507441722978d6924cc6_640.jpg"
-                alt="Teste"
-              />
+      {testimonials.length !== 0 && (
+        <ScrollMenu
+          data={testimonials.map(({ website, image, name }) => (
+            <ListItem key={`${name}-${website}`}>
+              <div>
+                <img src={image} alt={name} />
 
-              <Title>
-                <strong>Diego Silva</strong>
-                <span>http://diegosilva.com</span>
-              </Title>
+                <Title>
+                  <strong>{name}</strong>
+                  <span>{website}</span>
+                </Title>
 
-              <StarsCount>
-                <span>4.5</span>
-                <img src={starIcon} alt="Icone de recomendação" />
-              </StarsCount>
-            </div>
+                <StarsCount>
+                  <span>4.5</span>
+                  <img src={starIcon} alt="Icone de recomendação" />
+                </StarsCount>
+              </div>
 
-            <p>
-              “Com a Loocal conseguimos aumentar em 40% o número de vendas
-              durante a pandemia, ótimo serviço!”
-            </p>
-          </ListItem>
-        ))}
-        arrowLeft={<Arrow icon={FiArrowLeft} />}
-        arrowRight={<Arrow icon={FiArrowRight} />}
-        hideSingleArrow
-        wheel={false}
-        alignCenter={false}
-      />
+              <p>
+                “Com a Loocal conseguimos aumentar em 40% o número de vendas
+                durante a pandemia, ótimo serviço!”
+              </p>
+            </ListItem>
+          ))}
+          arrowLeft={<Arrow icon={FiArrowLeft} />}
+          arrowRight={<Arrow icon={FiArrowRight} />}
+          hideSingleArrow
+          wheel={false}
+          alignCenter={false}
+        />
+      )}
     </Container>
   );
 };
